9b68ec6a2ee22983acf5e52e9378de8fd131badd
Ihor Avdiunin <igor.avdiunin@gmail.com>
2024-02-29 18:19:00 +0200

    finish hw

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,49 +1,58 @@
 import HttpError from "../helpers/HttpError.js";
-import {
-  listContacts,
-  getContactById,
-  addContact,
-  updateContact,
-  removeContact,
-} from "../services/contactsServices.js";
+import { Contact } from "../models/contact.js";
 
-export const getAllContacts = async (req, res) => {
-  const contacts = await listContacts();
+export const getAllContacts = async (_, res) => {
+  const contacts = await Contact.find({}, "-createdAt -updatedAt");
   res.json(contacts);
 };
 
 export const getOneContact = async (req, res) => {
   const { id } = req.params;
-  const result = await getContactById(id);
+  const result = await Contact.findById(id, "-createdAt -updatedAt");
   if (!result) {
     throw HttpError(404, `Contacts with id=${id} not found`);
   }
   res.json(result);
 };
 
-export const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await removeContact(id);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.json(result);
-};
-
 export const createContact = async (req, res) => {
-  const result = await addContact(req.body);
-
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 export const updateContactById = async (req, res) => {
   const { id } = req.params;
-  const result = await updateContact(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, "Not found");
   }
   if (Object.keys(req.body).length === 0) {
     throw HttpError(400, "Body must have at least one field");
+  }
+  res.json(result);
+};
+
+export const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  if (Object.keys(req.body).length === 0) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+  res.json(result);
+};
+
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
   }
   res.json(result);
 };
---
+++ models/contact.js
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleSchemaValidationError } from "../helpers/handleSchemaValidationError.js";
+
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      required: [true, "Set email"],
+    },
+    phone: {
+      type: String,
+      unique: true,
+      required: [true, "Set phone"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleSchemaValidationError);
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool(),
+});
+
+export const updatePutContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+  favorite: Joi.bool(),
+});
+
+export const updatePatchContactSchema = Joi.object({
+  favorite: Joi.bool().required(),
+});
+
+export const Contact = model("contact", contactSchema);
